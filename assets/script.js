0ed30373106e512667349343320f771f6a53cc70
Wilsfried Di Majo <wilsfrieddm@gmail.com>
2023-11-06 10:18:06 +0100

    dots fonctionnels avec les arrows

--- assets/script.js
+++ assets/script.js
@@ -16,3 +16,47 @@ const slides = [
 		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
 	}
 ]
+
+let arrowLeft = document.querySelector("img.arrow_left");
+let arrowRight = document.querySelector("img.arrow_right");
+
+let i = 1;
+
+document.addEventListener('DOMContentLoaded', function() { /* So that 1st dot auto selected */
+	const firstDot = document.querySelector('span.dot:nth-child(1)');
+	firstDot.classList.add('dot_selected');
+});
+
+arrowLeft.addEventListener('click', function() {
+	i--;
+	console.log(i);
+	if (i === 0) {
+		i = 4;
+		updateSelectedDot();
+	} else {
+		updateSelectedDot();
+	}
+});
+
+arrowRight.addEventListener('click', function(){
+	i++;
+	console.log(i);
+	if ( i === 5) {
+		i = 1;
+		updateSelectedDot();
+	} else {
+		updateSelectedDot();
+	}
+});
+
+function updateSelectedDot() {
+	const dots = document.querySelectorAll('span.dot');
+
+	dots.forEach((dot, index) => {
+		if (index + 1 === i) {
+			dot.classList.add('dot_selected');
+		} else {
+			dot.classList.remove('dot_selected');
+		}
+	})
+}
